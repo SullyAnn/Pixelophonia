929267d02cd3eb19865750423f94c6a824106bb8
Adudree <addree@gmail.com>
2022-01-23 03:10:10 +0100

    [BACK] fonctions Concert

--- assets/classes/Admin.js
+++ assets/classes/Admin.js
@@ -27,3 +27,38 @@ export async function updateUploadImage(axios, formdata){
     await axios.$post(`http://127.0.0.1:3333/upload`,formdata,{headers : {'content-type':'multipart/form-data'}})
     .catch(error => {console.log(error)});
 }
+
+
+// ==================== CONCERTS ==================== //
+
+// get one concert by its id 
+export async function getConcert(axios, id) {
+    const concert = await axios.$get(`api/concert/${id}`)
+    return concert
+}
+
+// get all concerts 
+export async function getConcerts(axios) {
+    const concerts = await axios.$get(`api/concerts`)
+    console.log(concerts)
+    return concerts
+}
+
+// add one concert 
+export async function addConcert(axios, body){
+    await axios.$post(`api/concert`, JSON.stringify(body), {headers: { 'Content-Type': 'application/json' }})
+    .catch(error => { console.log(error) });
+    const concerts = await axios.$get('/api/questions')
+    return concerts
+}
+
+
+// delete one concert by its id 
+export async function deleteConcert(axios, id){
+    await axios.$delete(`api/concert/${id}`).catch(error => { console.log(error) })
+}
+
+// update one concert by its id
+export async function updateConcert(axios, id, body){
+    await axios.$put(`api/concert/${id}`, JSON.stringify(body), {headers: { 'Content-Type': 'application/json' }}).catch(error => { console.log(error) });
+}
