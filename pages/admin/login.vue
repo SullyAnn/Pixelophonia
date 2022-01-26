1d7ccdd3bae57303207b8ff2c7fa5f1a54177b3f
Sorimoutou <sori.sully@gmail.com>
2022-01-26 19:33:54 +0100

    [Login] front

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -2,13 +2,22 @@
   <section class="section">
     <div class="login">
       <div class="columns">
+        <div class="adminImage">
+          <img src="../../assets/images/Logo-carre-BD.png">
+          <h2>Administrateur</h2>
+        </div>
         <div class="column is-4 is-offset-4">
         <Notification :message="error" v-if="error"/>
             <form method="post" @submit.prevent="login">
-                <input v-model="email" type="text" name="email" class="labelChoice" required>
-                <input v-model="password" type="password" name="password" class="labelChoice" required>
-
-                  <button type="submit" class="btnMenu btnLogIn">Log In</button>
+                <div>
+                  <img src="../../assets/images/mail.png" class="icon">
+                  <input v-model="email" type="text" name="email" class="labelChoice" required>
+                </div>
+                <div id="passwordInput">
+                  <img src="../../assets/images/password.png" class="icon">
+                  <input v-model="password" type="password" name="password" class="labelChoice" required>
+                </div>
+                <button type="submit" class="btnMenu btnLogIn">Log In</button>
             </form>
         </div>
       </div>
@@ -57,8 +66,19 @@ export default {
 </script>
 <style>
 .btnLogIn{
-  width:20vw;
-  padding:10px !important;
+  width: 5vw;
+  padding: 10px !important;
+}
+form .btnMenu{
+  border: none;
+  margin: 0;
+  align-self: center;
+  margin-top: 25px;
+  background-color: #3D4D7C;
+  color: white;
+}
+form .btnMenu:hover{
+  color: #3D4D7C;
 }
 
 .login{
@@ -71,4 +91,58 @@ export default {
 .section{
   height:100vh;
 }
+.columns{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    height: 50vh;
+    padding: 40px;
+    border-radius: 40px;
+    background: rgba(255,255,255,0.5);
+  
+}
+.labelChoice{
+  background: unset;
+  padding: 16px;
+  font-size: 16px;
+  margin: 0;
+  width: -webkit-fill-available;
+}
+form{
+  background-color: unset;
+}
+form div{
+    margin: 0;
+    border-bottom: solid;
+    border-width: thin;
+    display: flex;
+    align-items: center;
+    width: auto;
+}
+.icon{
+  width: 16px;
+  height: fit-content;
+}
+
+.adminImage{
+  border-radius: 50px;
+  width: 150px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.adminImage img{
+  width: 100%;
+  border-radius: 150px;
+}
+.adminImage h2{
+  font-weight: 300;
+  font-size: 16px;
+}
+.column{
+  width: 40vh;
+}
+
 </style>
